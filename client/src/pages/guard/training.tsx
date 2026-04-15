fce93b10c731a4b5829c0e9283aa2c0302ad59ae
User <user@example.com>
2026-04-15 09:03:24 +0900

    fix(training): 지원하지 않던 YouTube Shorts URL 형식 추가

--- client/src/pages/guard/training.tsx
+++ client/src/pages/guard/training.tsx
@@ -68,7 +68,7 @@ const VideoPlayer = ({
     const intervalRef = useRef<NodeJS.Timeout>();
 
     const getYouTubeId = (url: string) => {
-        const regExp = /^.*(youtu.be\/|v\/|u\/\w\/|embed\/|watch\?v=|&v=)([^#&?]*).*/;
+        const regExp = /^.*(youtu.be\/|v\/|u\/\w\/|embed\/|watch\?v=|&v=|shorts\/)([^#&?]*).*/;
         const match = url.match(regExp);
         return (match && match[2].length === 11) ? match[2] : null;
     };
